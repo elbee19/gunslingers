8bf5bfe40900ba8459b4dbf05c3780c2a543ee65
yash <yash@ubuntu.(none)>
2013-09-15 15:27:15 -0400

    ---yash: shoot function that uses predictor

--- src/gunslinger/g2/Player.java
+++ src/gunslinger/g2/Player.java
@@ -17,11 +17,11 @@ public class Player extends gunslinger.sim.Player
     
     //Book-keeping variables
     PriorityQueue<PriorityTuple> priorityList;
-    
-	//bingyi-build the dynamic 2D table to record history
+    //bingyi-build the dynamic 2D table to record history
     List<List<Integer>> record = new ArrayList<List<Integer>>();
     //bingyi 
-
+    //yash
+    int prediction[][];
     //May not use these variables later
     //boolean shootable[];
     
@@ -44,7 +44,10 @@ public class Player extends gunslinger.sim.Player
          	record.add(new ArrayList<Integer>());
          }
          
-         priorityList=new  PriorityQueue<PriorityTuple>(10, new PriorityTupleComparator());
+         priorityList=new PriorityQueue<PriorityTuple>(10, new PriorityTupleComparator());
+         //yash
+         prediction=new int[this.nplayers][this.nplayers];
+         
     }
 
     
@@ -58,11 +61,12 @@ public class Player extends gunslinger.sim.Player
     //
     public int shoot(int[] prevRound, boolean[] alive)
     {
+    	
         if(prevRound==null)
         {
         	return -1; //Make love, not war
         }
-        
+         
         //printPrevRound(prevRound);
         int[] shotAt=new int[prevRound.length];
         
@@ -77,25 +81,158 @@ public class Player extends gunslinger.sim.Player
 		
         }
         
-        if(priorityList.size()==0)
-        	return -1;
+        //yash
+        //call prediction function here
+        prediction=predictNextRound(record, alive);
         
-        attentionSeekingPrint(priorityList.toString());
+        //Printing the prediction array returned
+        for(int i=0;i<prediction.length;i++)
+        {
+        	for(int j=0;j<prediction.length;j++)
+        	{
+        		System.out.print(prediction[i][j]);
+        	}
+        	System.out.println();
+        }
         
-        int myTarget=getMyTarget(shotAt);
-        priorityList.clear();
-    	//bingyi
-	printMatrix(record);
-	//bingyi		
-        return myTarget;
+//        int target=-1;
+//        Queue<Integer> likely_targets=new LinkedList<Integer>();
+        int popular_target[]=new int[prevRound.length];
+        int players[]=new int[prevRound.length];
+        
+        for(int i=0;i<players.length;i++)
+        {
+        	players[i]=i;
+        }
+        
+        //add the columns of prediction matrix to get player most likely to be shot at
+        for(int i=0;i<prediction.length;i++)
+        {
+        	for(int j=0;j<prediction.length;j++)
+        	{
+        		popular_target[i]+=prediction[j][i];
+        	}
+        }
+        
+        //find out who tried to shoot you in the previous round and add more likeliness to them
+        for(int i=0;i<prevRound.length;i++)
+        {
+        	//add more weight if an enemy shot you in the previous round
+        	if(prevRound[i]==this.id && isEnemy(i) && alive[i])
+        		popular_target[i]+=5;
+        	else if(prevRound[i]==this.id && isNeutral(i) && alive[i])
+        		popular_target[i]+=3; //add less weight if a neutral tried to shoot you
+        	//above conditions assume that friends don't shoot you
+        }
+        
+        //sort player and popular_target arrays to get sorted list of targets
+        for(int i=0;i<popular_target.length;i++)
+        {
+        	for(int j=0;j<popular_target.length-1;j++)
+        	{
+        		if(popular_target[j]<popular_target[j+1])
+        		{
+        			int temp=popular_target[j];
+        			popular_target[j]=popular_target[j+1];
+        			popular_target[j+1]=temp;
+        			temp=players[j];
+        			players[j]=players[j+1];
+        			players[j+1]=temp;
+        		}
+        	}
+        }
+        //We only need the most likely target, but I'm sorting the list just to get an idea of what our targets are like
+        
+        //printing players and their likeliness to be shot
+        for(int j=0;j<prediction.length;j++)
+    	{
+    		System.out.print("("+players[j]+","+popular_target[j]+") ");
+    	}
+    	System.out.println();
+        
+        
+        //shoot the most likely player if he is not a friend
+        for(int i=0;i<popular_target.length;i++)
+        {
+        	if((isEnemy(players[i]) || isNeutral(players[i])) && players[i]!=this.id && alive[players[i]])
+        	{
+        		return players[i];
+        	}
+        }
+        
+        //if no enemies or neutrals are to be shot at
+        return -1;
+        //yash
+        
+        
+        
+//        System.out.println("this is our hate most list\n");
+//        printListofArray(hate_most); 
+//        if(priorityList.size()==0)
+//        	return -1;
+//        
+//        attentionSeekingPrint(priorityList.toString());
+        
+//        int myTarget=getMyTarget(shotAt);
+//        priorityList.clear();
+//    	//bingyi
+//        printMatrix(record);
+//        //bingyi		
+//        return myTarget;
     }
     
+    //bingyi
+	public int[][] predictNextRound(List<List<Integer>> m, boolean[] alive)	
+	{
+		int columns = m.get(0).size();
+		int rows = m.size();
+		int[][] next_round = new int[rows][rows];
+		//if someone shoot the other last round, most likely the other will shot back next_round
+		for (int i=0; i<rows;i++){
+			if (m.get(i).get(columns-1) != -1) {
+				if (alive[i] && alive[m.get(i).get(columns-1)]){
+					next_round[m.get(i).get(columns-1)][i]=5;
+				}
+			}
+		}
+		//if someone shot someone before, and they hasn't died yet, high chance they will shot that person
+		for (int i=0; i< rows; i++) {
+			for (int j=0; j<columns;j++) {
+				if (m.get(i).get(j)!=-1 && next_round[i][m.get(i).get(j)]==0 && alive[i] && alive[m.get(i).get(j)]) {
+					next_round[i][m.get(i).get(j)]= 3;
+				}
+			}
+		}
+		//if someone shot them before, and that person hasn't diet yet, high chance they will shot back
+		for (int i=0;i<rows;i++){
+			for (int j=0;j<columns;j++){
+				if (m.get(i).get(j)!=-1 && alive[i] && alive[m.get(i).get(j)]){
+					if (next_round [m.get(i).get(j)][i]==0){
+						next_round[m.get(i).get(j)][i] =4;
+					}
+					
+				}
+			}
+		}
+		return next_round;
+	}
+	
+	public void printpredict(int[][] m){
+	for (int i=0; i<m[0].length; i++) { 
+		for (int j=0;j<m[0].length;j++) {
+    			System.out.println( m[i][j]+" ");
+  		} 
+  		System.out.println();
+  		}
+	}
+    //bingyi 
     public void attentionSeekingPrint(String s)
     {
     	System.out.println("----------------\n"+s+"\n-----------------");
     }
     
-    private int getMyTarget(int[] shotAt) {
+    private int getMyTarget(int[] shotAt) 
+    {
 		
     	/*PriorityTuple firstTuple=priorityList.remove();
     	if(shotAt[firstTuple.getPlayerId()]>0)
@@ -149,34 +286,31 @@ public class Player extends gunslinger.sim.Player
     	}
     	
     	//If n -> e, p4
-    	else if(!isFriend(shooter)&&!isEnemy(shooter)&&isEnemy(target)&&alive[target])
+    	else if(isNeutral(shooter)&&isEnemy(target)&&alive[target])
     	{
     		priorityList.add(new PriorityTuple(target,7));
     	}
     	
     	//If n -> f, p5
-    	else if(!isFriend(shooter)&&!isEnemy(shooter)&&isFriend(target)&&alive[target])
+    	else if(isNeutral(shooter)&&isFriend(target)&&alive[target])
     	{
     		if(alive[shooter])
     			priorityList.add(new PriorityTuple(shooter,6));
     	}
     	
     	//If e -> n, 
-    	else if(!isFriend(target)&&!isEnemy(target)&&isEnemy(shooter))
+    	else if(isNeutral(target)&&isEnemy(shooter))
     	{
     		if(alive[shooter])
     			priorityList.add(new PriorityTuple(shooter, 4));
     	}
     	
     	//If f -> n, 
-    	else if(isFriend(shooter)&&!isEnemy(target)&&!isFriend(target))
+    	else if(isFriend(shooter)&&isNeutral(target))
     	{
     		if(alive[target])
     			priorityList.add(new PriorityTuple(target, 5));
-    	}
-    	
-    	
-    	
+    	}	
 	}
 
 	public boolean isFriend(int pid)
@@ -199,6 +333,12 @@ public class Player extends gunslinger.sim.Player
     	return false;
     }
     
+    //yash
+    public boolean isNeutral(int pid)
+    {
+    	return !isEnemy(pid) && !isFriend(pid);
+    }
+    //yash
     
     public void printPrevRound(int[] a)
     {
@@ -210,6 +350,7 @@ public class Player extends gunslinger.sim.Player
     	
     	attentionSeekingPrint(s);
     }
+    
     public void printMatrix(List<List<Integer>> m)
     {
     //try{
